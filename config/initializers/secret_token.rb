46bf571c3cdd6c52ee28a1b87287192ff4d03e6c
Tobias Mollstam <tobias@mollstam.com>
2010-09-02 20:55:49 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biljett::Application.config.secret_token = '5146916f93863f2555975a8f67be8083099c87c949241b0937e5a3560d52bb2cf942db26e5d09380cdcda416383483fdc589ae8db7831923bc2522b84b55683e'
