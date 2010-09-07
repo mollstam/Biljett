3644d193d5f3863464a7aaec1d63f50671cb2e80
Tobias Mollstam <tobias@mollstam.com>
2010-09-07 20:30:08 +0200

    Implemented edit action for the tickets controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,7 +44,7 @@ class ProjectsController < ApplicationController
     def find_project
       @project = Project.find(params[:id])
       rescue ActiveRecord::RecordNotFound
-      flash[:error] = "The project you were looking for could not be found"
+      flash[:alert] = "The project you were looking for could not be found"
       redirect_to projects_path
     end
 end
